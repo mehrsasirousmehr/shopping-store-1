3389a3e4b4aba7ea29f85abf7792686335b0c0a2
Mehrsa <mhrs.project22@gmail.com>
2024-01-04 11:12:42 +0330

    validation

--- auth.js
+++ auth.js
@@ -1,6 +1,7 @@
 import { postData } from "./utils/httpReq.js";
 import { setCookie } from "./utils/cookie.js";
 import authHandler from "./utils/authorization.js";
+import validateForm from "./utils/validation.js";
 
 const inputsBox = document.querySelectorAll("input");
 const loginButton = document.querySelector("button");
@@ -10,6 +11,9 @@ const submitHandler = async (event) => {
 
     const username = inputsBox[0].value;
     const password = inputsBox[1].value;
+
+    const validation = validateForm(username, password);
+    if (!validation) return;
 
     const response = await postData("auth/login", { username, password });
 
