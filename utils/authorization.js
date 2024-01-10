4eca278a4027b2866591c11e824dc91f13751569
Mehrsa <mhrs.project22@gmail.com>
2024-01-10 18:16:41 +0330

    edit authorization and create home page

--- utils/authorization.js
+++ utils/authorization.js
@@ -5,11 +5,8 @@ const authHandler = () => {
     const cookie = getCookie();
     const url = location.href; // get url
 
-    if (cookie && url.includes("auth.html")) {
+    if ((cookie && url.includes("auth")) || (cookie && url.includes("dashboard"))) {
         location.assign("index.html");
-        return false;
-    } else if (!cookie && url.includes("dashboard")) {
-        location.assign("auth.html");
         return false;
     }
 };
