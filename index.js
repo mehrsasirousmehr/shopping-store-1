a4d5359d303bd972ba0e8022322d8d683fd3996c
Mehrsa <mhrs.project22@gmail.com>
2024-01-11 16:45:58 +0330

    showProducts

--- index.js
+++ index.js
@@ -1,8 +1,40 @@
 import { getCookie } from "./utils/cookie.js";
 import { getData } from "./utils/httpReq.js";
+import { shortenText } from "./utils/stringFunc.js";
 
 const loginButton = document.getElementById("login");
 const dashboardButton = document.getElementById("dashboard");
+const mainContent = document.getElementById("products");
+
+const showProducts = (products) => {
+    mainContent.innerHTML = "";
+
+    products.forEach((product) => {
+        const jsx = `
+            <div>
+                <img alt=${product.title} src=${product.image} />
+                <h4>${shortenText(product.title)}</h4>
+                <div id="price">
+                    <p>$ ${product.price}</p>
+                    <button>
+                        Buy
+                        <i class="fa-solid fa-cart-shopping"></i>
+                    </button>
+                </div>
+                <div id="rate">
+                    <i class="fa-solid fa-star"></i>
+                    <span>${product.rating.rate}</span>
+                </div>
+                <div id="count">
+                    <i class="fa-solid fa-user"></i>
+                    <span>${product.rating.count}</span>
+                </div>
+            </div>
+        `;
+
+        mainContent.innerHTML += jsx;
+    });
+};
 
 // show login or dashboard button
 const init = async () => {
@@ -17,7 +49,7 @@ const init = async () => {
     }
 
     const allProducts = await getData("products");
-    console.log(allProducts);
+    showProducts(allProducts);
 };
 
 document.addEventListener("DOMContentLoaded", init);
