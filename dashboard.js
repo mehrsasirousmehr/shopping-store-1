7dde4a544c6caeccfea82874493da67e49e883e7
Mehrsa <mhrs.project22@gmail.com>
2024-01-11 23:16:25 +0330

    show users data

--- dashboard.js
+++ dashboard.js
@@ -5,12 +5,43 @@ const mainContent = document.getElementById("container");
 
 const renderUsers = (users) => {
     mainContent.innerHTML = "";
+
+    // show users data
+    users.forEach((user) => {
+        const jsx = `
+            <div id="card">
+                <h3>${user.id}</h3>
+                <div>
+                    <p><i class="fa-solid fa-user"></i>Name:</p>
+                    <span>${user.name.firstname} ${user.name.lastname}</span>
+                </div>
+                <div>
+                    <p><i class="fa-solid fa-paperclip"></i>Username:</p>
+                    <span>${user.username}</span>
+                </div>
+                <div>
+                    <p><i class="fa-solid fa-envelope"></i>Email:</p>
+                    <span>${user.email}</span>
+                </div>
+                <div>
+                    <p><i class="fa-solid fa-Phone"></i>Phone:</p>
+                    <span>${user.phone}</span>
+                </div>
+                <div>
+                    <p><i class="fa-solid fa-location-dot"></i>Address:</p>
+                    <span>${user.address.city} - ${user.address.street} - ${user.address.zipcode}</span>
+                </div>
+            </div>
+        `;
+
+        mainContent.innerHTML += jsx;
+    });
 };
 
 const init = async () => {
     // check user access level in different pages
     authHandler();
-    
+
     // get users data
     const users = await getData("users");
     renderUsers(users);
