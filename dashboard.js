e2a89e6985148eb477b08eaa3fd8a5865f773fcc
Mehrsa <mhrs.project22@gmail.com>
2024-01-11 23:02:39 +0330

    get users data

--- dashboard.js
+++ dashboard.js
@@ -1,8 +1,19 @@
 import authHandler from "./utils/authorization.js";
+import { getData } from "./utils/httpReq.js";
 
-// check user access level in different pages
-const init = () => {
+const mainContent = document.getElementById("container");
+
+const renderUsers = (users) => {
+    mainContent.innerHTML = "";
+};
+
+const init = async () => {
+    // check user access level in different pages
     authHandler();
+    
+    // get users data
+    const users = await getData("users");
+    renderUsers(users);
 };
 
 document.addEventListener("DOMContentLoaded", init);
